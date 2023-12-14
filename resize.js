577b2ca41a04e77657b48dd942021122842686d9
tejasdere <149783500+tejasdere@users.noreply.github.com>
2023-12-14 15:45:08 +0530

    Update resize.js

--- resize.js
+++ resize.js
@@ -1,1 +1,11 @@
+const resizeIframe = () => {
+  const iframes = document.querySelectorAll(".grid-item iframe");
+  iframes.forEach(iframe => {
+    // Dynamically calculate height based on graph content
+    iframe.style.height = iframe.contentWindow.document.body.scrollHeight + "px";
+  });
+};
+
+window.addEventListener("load", resizeIframe);
+window.addEventListener("resize", resizeIframe);
 
