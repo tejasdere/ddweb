e7fd7fe51ae07a292944948d195de42afa220368
tejasdere <149783500+tejasdere@users.noreply.github.com>
2023-12-14 15:20:48 +0530

    Update resize.js

--- resize.js
+++ resize.js
@@ -1,1 +1,20 @@
+$(function() {
+  $(".resize-handle").mousedown(function(event) {
+    var iframeWrapper = $(this).parent();
+    var iframe = iframeWrapper.find("iframe");
+    var startWidth = iframe.width();
+    var startHeight = iframe.height();
+    var startX = event.clientX;
+    var startY = event.clientY;
+
+    $(document).mousemove(function(event) {
+      var deltaX = event.clientX - startX;
+      var deltaY = event.clientY - startY;
+      iframe.width(startWidth + deltaX);
+      iframe.height(startHeight + deltaY);
+    }).mouseup(function() {
+      $(document).unbind("mousemove");
+    });
+  });
+});
 
