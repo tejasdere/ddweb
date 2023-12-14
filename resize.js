dc36f9b2668310122f6151df4290e82627b91e17
tejasdere <149783500+tejasdere@users.noreply.github.com>
2023-12-14 14:35:24 +0530

    Update resize.js

--- resize.js
+++ resize.js
@@ -1,10 +1,1 @@
-$(function() {
-  $(window).on('resize', function() {
-    var containerWidth = $('.container').width();
-    var graphWrapperWidth = containerWidth * 0.8;
-    if (containerWidth < 768) {
-      graphWrapperWidth = containerWidth * 0.6;
-    }
-    $('.graph-wrapper').width(graphWrapperWidth);
-  }).resize();
-});
+
